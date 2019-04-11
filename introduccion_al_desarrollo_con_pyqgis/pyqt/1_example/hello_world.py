c40448c5c4a1e1d6720bd40261b77df8e6e9f403
Leo Cardona <leocardonapiedrahita@gmail.com>
2019-04-11 15:51:56 -0500

    create simple hello word message

--- introduccion_al_desarrollo_con_pyqgis/pyqt/1_example/hello_world.py
+++ introduccion_al_desarrollo_con_pyqgis/pyqt/1_example/hello_world.py
@@ -1,30 +1,8 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication,
-                             QMainWindow,
-                             QLabel,
-                             QGridLayout,
-                             QWidget)
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-        self.setMinimumSize(400, 400)
-        self.setWindowTitle(".:: Hola mundo ::.")
-
-        centralWidget = QWidget(self)
-        self.setCentralWidget(centralWidget)
-
-        gridLayout = QGridLayout(self)
-        centralWidget.setLayout(gridLayout)
-
-        message = QLabel('Hola Grupo de Usuarios QGIS Colombia')
-        message.setAlignment(Qt.AlignCenter)
-        gridLayout.addWidget(message, 0, 0)
-
-
-if __name__ == "__main__":
-    app = QApplication([])
-    mainWindow = MainWindow()
-    mainWindow.show()
-    app.exec_()
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle('Mi primer aplicación con PyQT')
+QLabel('Hola mundo', window)
+window.show()
+app.exec_()
