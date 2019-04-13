d1fb82d1fa6b6b65e5ef0c849cad5a8eee4e0cce
gacarrillor <geotux_tuxman@linuxmail.org>
2019-04-12 19:57:37 -0500

    Add standalone script to read layer properties

---
+++ introduccion_al_desarrollo_con_pyqgis/pyqgis/08_standalone_layer_properties.py
@@ -0,0 +1,16 @@
+from qgis.core import (QgsApplication, 
+                       QgsVectorLayer)
+
+# See https://gis.stackexchange.com/a/155852/4972 for details about the prefix 
+QgsApplication.setPrefixPath('C:\OSGeo4W\apps\qgis-ltr', True)
+qgs = QgsApplication([], False)
+qgs.initQgis()
+
+airport_layer = QgsVectorLayer("C:\pyqgis\geodata\sample_data.gpkg|layername=Airports", "airport", "ogr")
+
+print(airport_layer.isValid())
+print(airport_layer.featureCount())
+for f in airport_layer.getFeatures():
+    print(f['name'], f.geometry().asWkt())
+
+qgs.exitQgis()
