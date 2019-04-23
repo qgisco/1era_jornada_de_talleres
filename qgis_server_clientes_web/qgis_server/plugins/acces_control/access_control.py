c31dd39c3ddb8c096358609647ad62472571e7f4
gacarrillor <geotux_tuxman@linuxmail.org>
2019-04-23 00:18:56 -0500

    Update access_control plugin for QGIS Server

--- qgis_server_clientes_web/qgis_server/plugins/acces_control/access_control.py
+++ qgis_server_clientes_web/qgis_server/plugins/acces_control/access_control.py
@@ -6,29 +6,33 @@ class RestrictedAccessControl(QgsAccessControlFilter):
 
     def layerFilterExpression(self, layer):
         """ Return an additional expression filter """
-        return "intersects_bbox( $geometry, geom_from_wkt('LINESTRING(-8245386.2 525874.5, -8245321.2 525920.4)' ) )"
-        # return super(RestrictedAccessControl, self).layerFilterExpression(layer)
+        #return "intersects_bbox( $geometry, geom_from_wkt('LINESTRING(-8245386.2 525874.5, -8245321.2 525920.4)' ) )"
+        return super(RestrictedAccessControl, self).layerFilterExpression(layer)
 
     def layerFilterSubsetString(self, layer):
         """ Return an additional subset string (typically SQL) filter """
-        # return """ "fid" = '473' """
+        #return """ "fid" = '473' """
         return super(RestrictedAccessControl, self).layerFilterSubsetString(layer)
 
     def layerPermissions(self, layer):
         """ Return the layer rights """
-        if layer.name() == 'construccion':
-            rights = QgsAccessControlFilter.LayerPermissions()
-            rights.canRead = rights.canInsert = rights.canUpdate = rights.canDelete = False
-            return rights
+        #if layer.name() == 'construccion':
+        #    rights = QgsAccessControlFilter.LayerPermissions()
+        #    rights.canRead = rights.canInsert = rights.canUpdate = rights.canDelete = False
+        #    return rights
 
         return super(RestrictedAccessControl, self).layerPermissions(layer)
 
     def authorizedLayerAttributes(self, layer, attributes):
         """ Return the authorised layer attributes """
-        return ["area_terreno", "avaluo_terreno"] 
+        #return ["area_terreno", "avaluo_terreno"] 
+        return super(RestrictedAccessControl, self).authorizedLayerAttributes(layer, attributes)
 
     def allowToEdit(self, layer, feature):
         """ Are we authorise to modify the following geometry """
+        #if layer.name() == 'construccion':
+        #    return feature['fid'] >= 67 and feature['fid'] <= 80
+            
         return super(RestrictedAccessControl, self).allowToEdit(layer, feature)
 
     def cacheKey(self):
@@ -39,3 +43,4 @@ class AccessControl:
     def __init__(self, serverIface):
         # Save reference to the QGIS server interface
         serverIface.registerAccessControl( RestrictedAccessControl(serverIface), 100 )
+
